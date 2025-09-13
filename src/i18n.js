e6e0580d00e0c121a4bcc6cf859dd386e73247ed
stanimia81 <tristani_81@yahoo.de>
2025-09-13 17:20:52 +0200

    Create i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,11 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translations from './translations';
+
+i18n.use(initReactI18next).init({
+  resources: translations,
+  lng: 'de',
+  interpolation: { escapeValue: false }
+});
+
+export default i18n;
