bbf61140b1a226bb107c041850d9bedb9a682905
stanimia81 <tristani_81@yahoo.de>
2025-09-18 15:58:11 +0200

    Create translations.js

---
+++ src/translations.js
@@ -0,0 +1,14 @@
+const translations = {
+  de: {
+    translation: {
+      welcome: "Willkommen auf meiner Website",
+    },
+  },
+  en: {
+    translation: {
+      welcome: "Welcome to my website",
+    },
+  },
+};
+
+export default translations;
